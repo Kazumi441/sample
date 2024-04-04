df6c16e187cc15e1db4b82e9957c571c1e308ae8
Kazumi441 <164310364+Kazumi441@users.noreply.github.com>
2024-04-05 00:02:45 +0700

    Add files via upload

--- 宿題/app.py
+++ 宿題/app.py
@@ -1,18 +1,7 @@
 import streamlit as st
-from pages import asobi, basic, share, tenki
+
 
 
 st.title('アプリ宿題シリーズ')
 st.write('ここに宿題を置きます')
 
-st.title("Navigation")
-page = st.radio("Go to", ["asobi", "基礎", "株価可視化", "天気"])
-
-if page == "asobi":
-    asobi.show()
-elif page == "基礎":
-    basic.show()
-elif page == "株価可視化":
-    share.show()
-elif page == "天気":
-    tenki.show()
